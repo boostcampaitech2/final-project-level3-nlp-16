2504a88a6deafed9b84626ffb793f763e4b0fa41
Sangryu Park <psrpsj@gmail.com>
2021-12-12 13:59:09 +0000

    huggingface dataset implemented

--- code/generation/train.py
+++ code/generation/train.py
@@ -1,6 +1,5 @@
 from transformers import (
     AutoTokenizer,
-    AutoConfig,
     HfArgumentParser,
     GPT2LMHeadModel,
     Trainer,
@@ -8,28 +7,22 @@ from transformers import (
 )
 from arguments import (
     ModelArguments,
-    DataTrainingArguments,
     TrainingArguments,
 )
 from data import CustomDataset
-from preprocess import (
-    create_data_normal,
-    create_data_cosine,
-)
+from preprocess import create_preprocess_data
 
 
 def main():
     parser = HfArgumentParser(
         (
             ModelArguments,
-            DataTrainingArguments,
             TrainingArguments,
         )
     )
-    (model_args, data_args, training_args) = parser.parse_args_into_dataclasses()
+    (model_args, training_args) = parser.parse_args_into_dataclasses()
 
     print(f"backbone model is {model_args.model_name_or_path}")
-    print(f"data is {data_args.dataset_name}")
 
     set_seed(training_args.seed)
 
@@ -43,19 +36,14 @@ def main():
         sep_token="<sep>",
         use_fast=True,
     )
-    config = AutoConfig.from_pretrained(
-        pretrained_model_name_or_path=model_args.model_name_or_path
-    )
+
     model = GPT2LMHeadModel.from_pretrained(model_args.model_name_or_path)
     model.resize_token_embeddings(len(tokenizer))
     model.train()
 
-    if data_args.dataset_type == "cosine":
-        tag_dataset = create_data_cosine(data_args.dataset_name, data_args.cosine_rate)
-    else:
-        tag_dataset = create_data_normal(data_args.dataset_name)
-
+    tag_dataset = create_preprocess_data()
     dataset = CustomDataset(tag_dataset["data"].tolist(), tokenizer)
+
     trainer = Trainer(
         model=model,
         args=training_args,

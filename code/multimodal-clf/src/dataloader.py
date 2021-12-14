5757bdd773830a05ac48235e8c4108b35a6b61f9
choi-yunsung <jws02025@gmail.com>
2021-12-14 17:00:19 +0000

    feat(mmclf): soft votting

--- code/multimodal-clf/src/dataloader.py
+++ code/multimodal-clf/src/dataloader.py
@@ -1,117 +1,87 @@
-"""Tune Model.
-
-- Author: Junghoon Kim, Jongkuk Lim, Jimyeong Kim
-- Contact: placidus36@gmail.com, lim.jeikei@gmail.com, wlaud1001@snu.ac.kr
-- Reference
-    https://github.com/j-marple-dev/model_compression
-"""
-import glob
-import os
 from typing import Any, Dict, List, Tuple, Union
 
 import torch
-import yaml
-from torch.utils.data import Dataset, DataLoader, random_split
-from torchvision.datasets import ImageFolder, VisionDataset
+from torch.utils.data import DataLoader
+
+from datasets import DatasetDict
+from transformers import AutoTokenizer
+
 from PIL import Image
-import pandas as pd
-import pickle
-
-
-with open('dict_label_to_num', 'rb') as f:
-    _dict_label_to_num = pickle.load(f)
-
-class TrainDataset(torch.utils.data.Dataset):
-    def __init__(self, config: Dict[str, Any], dataframe, tokenized_txt):
-        dataset_name=config["DATASET"]
-        img_size=config["IMG_SIZE"]
-        transform_train=config["AUG_TRAIN"]
-        transform_train_params=config["AUG_TRAIN_PARAMS"]
-        if not transform_train_params:
-            transform_train_params = dict()
-
-        self.dataframe = dataframe
-
-        # preprocessing policies
-        self.transform_train = getattr(
-            __import__("src.augmentation.policies", fromlist=[""]),
-            transform_train,
-        )(dataset=dataset_name, img_size=img_size, **transform_train_params)
-
-        self.tokenized_txt = tokenized_txt
-        self.labels = list(self.dataframe["category"].apply(lambda x: _dict_label_to_num[str(x)]))
-
-
-    def __getitem__(self, idx):
-        item = {key: val[idx].clone().detach() for key, val in self.tokenized_txt.items()}
-        
-        image = Image.open(self.dataframe.loc[idx, "img"]).convert('RGB')
-        image = self.transform_train(image)
-        item["img"] = image
-
-        labels = torch.tensor(self.labels[idx])
-        return item, labels
-
-    def __len__(self):
-        return len(self.labels)
-
-
-class TestDataset(torch.utils.data.Dataset):
-    def __init__(self, config: Dict[str, Any], dataframe, tokenized_txt):
-        dataset_name=config["DATASET"]
-        img_size=config["IMG_SIZE"]
-        transform_test=config["AUG_TEST"]
-        transform_test_params=config.get("AUG_TEST_PARAMS")
-        if not transform_test_params:
-            transform_test_params = dict()
-
-        self.dataframe = dataframe
-
-        # preprocessing policies
-        self.transform_test= getattr(
-            __import__("src.augmentation.policies", fromlist=[""]),
-            transform_test,
-        )(dataset=dataset_name, img_size=img_size, **transform_test_params)
-
-        self.tokenized_txt = tokenized_txt
-        self.labels = list(self.dataframe["category"].apply(lambda x: _dict_label_to_num[str(x)]))
-
-
-    def __getitem__(self, idx):
-        item = {key: val[idx].clone().detach() for key, val in self.tokenized_txt.items()}
-        
-        image = Image.open(self.dataframe.loc[idx, "img"]).convert('RGB')
-        image = self.transform_test(image)
-        item["img"] = image
-
-        labels = torch.tensor(self.labels[idx])
-        return item, labels
-
-    def __len__(self):
-        return len(self.labels)
+from io import BytesIO
 
 
 def get_dataloader(
-    train_dataset: Dataset,
-    val_dataset: Dataset,
-    batch_size: int,
-) -> Tuple[DataLoader, DataLoader]:
-    """Get dataloader for training and testing."""
+    datasets: DatasetDict,
+    data_config: Dict[str, Any],
+    model_config: Dict[str, Any],
+) -> Tuple[DataLoader, DataLoader, DataLoader]:
+    """Get dataloader for train, validation, test"""
 
+    # Load image transforms
+    transform_train_params=data_config["AUG_TRAIN_PARAMS"]
+    if not transform_train_params:
+        transform_train_params = dict()
+
+    transform_train = getattr(
+        __import__("src.augmentation.policies", fromlist=[""]),
+        data_config["AUG_TRAIN"],
+    )(img_size=data_config["IMG_SIZE"], **transform_train_params)
+    transform_test = getattr(
+        __import__("src.augmentation.policies", fromlist=[""]),
+        data_config["AUG_TEST"],
+    )(img_size=data_config["IMG_SIZE"])
+
+    # Load text tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_config["txt_backbone"])
+
+    def text_to_token(text):
+        return tokenizer(text, return_tensors="pt", max_length=32, padding="max_length", truncation="only_first")
+        
+    def apply_transform_train(example_batch):
+        # apply image transform
+        example_batch["pixel_values"] = [
+            transform_train(bytes_to_pil(b)) for b in example_batch.pop("img_bytes")
+        ]
+        # apply text tokenizer
+        for key, val in text_to_token(example_batch.pop("title")).items():
+            example_batch[key] = val
+        return example_batch
+
+    def apply_transform_test(example_batch):
+        # apply image transform
+        example_batch["pixel_values"] = [
+            transform_test(bytes_to_pil(b)) for b in example_batch.pop("img_bytes")
+        ]
+        # apply text tokenizer
+        for key, val in text_to_token(example_batch.pop("title")).items():
+            example_batch[key] = val
+        return example_batch
+
+    # Create dataloader
     train_loader = DataLoader(
-        dataset=train_dataset,
+        dataset=datasets["train"].with_transform(apply_transform_train),
         pin_memory=(torch.cuda.is_available()),
         shuffle=True,
-        batch_size=batch_size,
+        batch_size=data_config["TRAIN_BATCH_SIZE"],
         num_workers=10,
         drop_last=True
     )
     valid_loader = DataLoader(
-        dataset=val_dataset,
+        dataset=datasets["valid"].with_transform(apply_transform_test),
         pin_memory=(torch.cuda.is_available()),
         shuffle=False,
-        batch_size=batch_size,
-        num_workers=5
+        batch_size=data_config["VALID_BATCH_SIZE"],
+        num_workers=5,
+    )
+    test_loader = DataLoader(
+        dataset=datasets["test"].with_transform(apply_transform_test),
+        pin_memory=(torch.cuda.is_available()),
+        shuffle=False,
+        batch_size=data_config["VALID_BATCH_SIZE"],
+        num_workers=5,
     )
 
-    return train_loader, valid_loader
+    return train_loader, valid_loader, test_loader
+
+def bytes_to_pil(b):
+    return Image.open(BytesIO(b)).convert('RGB')
--- code/multimodal-clf/src/utils/common.py
+++ code/multimodal-clf/src/utils/common.py
@@ -23,3 +23,35 @@ def seed_everything(seed) :
     torch.backends.cudnn.benchmark = False
     np.random.seed(seed)
     random.seed(seed)
+
+
+def model_info(model, verbose=False):
+    """Print out model info."""
+    n_p = sum(x.numel() for x in model.parameters())  # number parameters
+    n_g = sum(
+        x.numel() for x in model.parameters() if x.requires_grad
+    )  # number gradients
+    if verbose:
+        print(
+            "%5s %40s %9s %12s %20s %10s %10s"
+            % ("layer", "name", "gradient", "parameters", "shape", "mu", "sigma")
+        )
+        for i, (name, p) in enumerate(model.named_parameters()):
+            name = name.replace("module_list.", "")
+            print(
+                "%5g %60s %9s %12g %20s %10.3g %10.3g"
+                % (
+                    i,
+                    name,
+                    p.requires_grad,
+                    p.numel(),
+                    list(p.shape),
+                    p.mean(),
+                    p.std(),
+                )
+            )
+
+    print(
+        f"Model Summary: {len(list(model.modules()))} layers, "
+        f"{n_p:,d} parameters, {n_g:,d} gradients"
+    )
